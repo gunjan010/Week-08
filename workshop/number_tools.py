be477f12b68a8a7970363fca5604eed49d0b7a9c
Gunjan <gunjanrazk@gmail.com>
2026-04-13 13:55:04 +0930

    Implement test for average function

--- workshop/number_tools.py
+++ workshop/number_tools.py
@@ -4,3 +4,17 @@ def is_even(n):
         return n % 2 == 0
     else:
         raise TypeError("Only int is allowed.")
+    
+def average(numbers):
+    '''Checks if the passed argument is list, not empty, and every item is int/float 
+       and raises Error if not. Returns the average of items in the list. '''
+    total = 0
+    if not isinstance(numbers, list):
+        raise TypeError("Argument must be a list.")
+    if len(numbers) == 0:
+        raise ValueError("List must not be empty.")
+    for number in numbers:
+        if not isinstance(number, (int, float)):
+            raise TypeError("All items must be int or float.")
+        total += number
+    return total / len(numbers)
--- workshop/number_tools_unittest.py
+++ workshop/number_tools_unittest.py
@@ -13,4 +13,12 @@ class TestNumberTools(unittest.TestCase):
         # Test errors
         self.assertRaises(TypeError, is_even, 3.5)
 
+    def test_average(self):
+        self.assertEqual(average([2,4,6,8]), 5)
+        self.assertAlmostEqual(average([1.5,2.5,3.5]), 2.5)
+        self.assertRaises(TypeError, average, "not a list")
+        self.assertRaises(ValueError, average, [])
+        self.assertRaises(TypeError, average, [1, 2, "three"])
+        
+
 unittest.main()
