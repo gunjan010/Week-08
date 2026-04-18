4afdcb3b2f50905d26dfaffa269a277a540afc4a
Gunjan <gunjanrazk@gmail.com>
2026-04-18 19:57:18 +0930

    Implement test for area function in Rectangle class

--- practical/rectangle.py
+++ practical/rectangle.py
@@ -1,6 +1,21 @@
 from shape2D import Shape2D
 
-class rectangle(Shape2D):
+class Rectangle(Shape2D):
     '''returns the area of rectangle by making use of abstract method(area) from Shape2D class'''
     def area(self):
         return self.width * self.height
+    
+    def __str__(self):
+        return f"Rectangle {self.width} X {self.height}\nArea: {self.area()}"
+
+# test code
+if __name__ == "__main__":
+    shapes = [
+        Rectangle(3, 4),
+        Rectangle(5, 6),
+        Rectangle(2, 3)
+    ]
+    for shape in shapes:
+        print(shape)
+        pass
+    pass
